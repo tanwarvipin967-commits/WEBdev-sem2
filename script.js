bd93f104332a4c1a74af782bc52f3d3215ab3a0b
Vipin Tanwar <tanwarvipin967@gmail.com>
2026-01-19 12:39:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// let obj={
+//     name:"Vipin",
+//     age:"19",
+// }
+// let arr={
+//     name:"yash",
+//     section:"FSD",
+// const button=document.getElementsByClassName("btn_click")
+// button[0].addEventListener("click",Btn_click)
+// function Btn_click(){
+//     alert("button clicked")
+// }
+
+// const stopButton=document.getElementsByClassName("Btn_stop")
+// stopButton[0].addEventListener("click",Stop_click)
+// function Stop_click(){
+//     button[0].removeEventListner("click",Btn_click)
+//     alert("Event Listner Removed")
+// // }
+// const btn=document.querySelector(".btn_click")
+// btn.addEventListener('keyup',(event)=>{
+//     console.log(event.key)
+// })
+
+// const form=document.querySelector("form")
+// form.addEventListener('submit',(event)=>{
+//     event.preventDefault()
+//     console.log(event.srcElement[0].value)
+//     console.log(event.srcElement[1].value)
+//     console.log(event)
+//     console.log("Button Clicked")
+// })
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const form=document.querySelector("form")
+// form.addEventListener('submit',(event)=>{
+//     event.preventDefault()
+//     console.log(event.srcElement[0].value)
+//     console.log(event.srcElement[1].value)
+//     console.log(event)
+//     console.log("Button Clicked")
+// })
