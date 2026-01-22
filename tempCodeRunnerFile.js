dbdb93d173b8e2656417ca0ae58104f578f7484c
Vipin Tanwar <tanwarvipin967@gmail.com>
2026-01-22 14:37:39 +0530

    First code

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const form=document.querySelector("form")
-// form.addEventListener('submit',(event)=>{
-//     event.preventDefault()
-//     console.log(event.srcElement[0].value)
-//     console.log(event.srcElement[1].value)
-//     console.log(event)
-//     console.log("Button Clicked")
-// })
